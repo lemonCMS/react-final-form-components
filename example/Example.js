7e0b844e8cbbe453de7647c2770c6d2f3ad2d2de
raymond <rsvvuuren@hotmail.com>
2018-04-07 00:44:10 +0200

    fix timestamp incomp

--- example/Example.js
+++ example/Example.js
@@ -82,6 +82,7 @@ class Example extends React.Component {
             }}
             onSubmit={onSubmit}
             initialValues={{
+              'birthday': 1463436000,
               'resource': [1],
               'choose-3': '1',
               'resource': [1],
@@ -106,7 +107,8 @@ class Example extends React.Component {
                   <option value="1">Something else</option>
                 </Dropdown>
               )}/>
-              <DateTime label="birthday" name={"birthday"} {...size} conf={{timeFormat: false}}/>
+              <DateTime label="birthday" name={"birthday"} {...size} conf={{timeFormat: false, unix: true}}/>
+              <DateTime label="birthday2" name={"birthday2"} {...size} />
               <Select label="Choose" name="choose-1" {...size}>
                 <option value="0">Something</option>
                 <option value="1">Something else</option>
--- src/Components/DateTime.jsx
+++ src/Components/DateTime.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _isFunction from 'lodash/isFunction';
+import _omitBy from 'lodash/omitBy';
 import ReactDateTime from 'react-datetime';
+import moment from 'moment';
 
 class ContextBinder extends React.Component {
+
+  state = {
+    value: ''
+  };
+
+  componentWillMount() {
+    if (this.props.input.value && this.props.input.value !== '' && this.props.field.conf && this.props.field.conf.unix) {
+      this.setState({value: moment.unix(this.props.input.value)}, () => {
+        this.props.onChange(this.state.value);
+      });
+    } else {
+      this.setState({
+        value: moment(this.props.input.value)
+      }, () => {
+        this.props.onChange(this.state.value);
+      });
+    }
+  }
 
   render() {
     if (this.context.isStatic || this.props.field.static) {
@@ -12,7 +32,6 @@ class ContextBinder extends React.Component {
       );
     }
 
-
     const inputProps = {
       disabled: false
     };
@@ -20,8 +39,16 @@ class ContextBinder extends React.Component {
       inputProps.disabled = this.context.checkCondition(this.props.field.disabled);
     }
 
+    const newProps = _omitBy(this.props.input, ['value', 'onChange', 'onBlur', 'onFocus']);
+    newProps.onChange = (value) => {
+      this.setState({value: value}, () => {
+        this.props.input.onChange(value);
+      });
+    };
+    newProps.value = this.state.value;
+
     return (
-      <ReactDateTime {...this.props.input} inputProps={inputProps} {...this.props.field.conf} />
+      <ReactDateTime {...newProps} inputProps={inputProps} {...this.props.field.conf} />
     );
   }
 }
