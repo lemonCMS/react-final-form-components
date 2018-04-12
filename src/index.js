deab636b803c3419fd2fb38746c27b41cffc8dfb
raymond <rsvvuuren@hotmail.com>
2018-04-12 11:22:44 +0200

    fix broken export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Form from './Form';
 
+export Button from './Bs/Button';
 export Checkbox from './Bs/Checkbox';
 export Complex from './Bs/Complex';
 export ComplexRow from './Bs/ComplexRow';
 export DateTime from './Bs/DateTime';
-export DropDown from './Bs/DropDown';
+export DropDown from './Bs/Dropdown';
 export Input from './Bs/Input';
 export Message from './Bs/Message';
 export Plupload from './Bs/Plupload';
